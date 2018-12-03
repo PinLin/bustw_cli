44938c6afa6cffabd80a87a786bfaafedc251269
Pin Lin <moneycat711@gmail.com>
2018-12-03 22:00:04 +0800

    Auto install requirements before running

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
+from pip._internal import main as pip_main
+
+try:
+    with open(sys.path[0] + '/requirements.txt') as f:
+        mods = f.read().strip().split('\n')
+    list(map(__import__, mods))
+
+except FileNotFoundError:
+    pass
+
+except ModuleNotFoundError:
+    pip_main(['install', '-r', 'requirements.txt'])
+
 from bustw_cli import app
 
 
