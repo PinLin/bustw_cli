133bc527a217e77ec545dc80e771ebf49d160a80
Pin Lin <moneycat711@gmail.com>
2018-12-05 13:17:10 +0800

    Refactor init.py

--- bustw_cli/init.py
+++ bustw_cli/init.py
@@ -1,23 +1,24 @@
 from .utils.bustw import Bustw
 
+bustw = Bustw()
+
 
 class Init:
     def __init__(self, data: dict):
         self.__data = data
-        self.__bustw = Bustw()
 
     def load_cities(self):
         """讀取城市資料"""
 
-        result = {}
-        for item in self.__bustw.get_city():
-            result[item['key']] = {
+        for item in bustw.get_city():
+            self.__data['cities'][item['key']] = {
                 'name': item['name'],
                 'enable': True,
             }
-        self.__data['cities'] = result
 
     def main(self):
+        self.__data['cities'] = {}
+
         self.load_cities()
 
         return 'old_choose'
