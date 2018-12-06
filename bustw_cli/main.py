0e9e2f5b38c27c6dc3d1d97161879986db5d9398
Pin Lin <moneycat711@gmail.com>
2018-12-06 09:03:49 +0800

    Finished main.py

--- bustw_cli/main.py
+++ bustw_cli/main.py
@@ -7,13 +7,20 @@ class Main:
     def __init__(self, data: dict):
         self.__data = data
 
-    def main(self):
+    def choose(self):
+        args = []
+        self.__data['args'] = args
+
         print()
         print("直接按下 Enter 以進入設定頁")
         print("或是輸入想要查詢的路線（範例：72、680、幹線）")
+        args.append(input("(bustw)? "))
 
-        select = input("(bustw)? ")
-        if select == '':
+    def main(self):
+        if not self.__data.get('args'):
+            self.choose()
+
+        if self.__data['args'][0] == '':
             return 'setting'
 
         return 'old_choose'
