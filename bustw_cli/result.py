ebc727d9aa97712a0a20a4bc2a59ed16ec0577fe
Pin Lin <moneycat711@gmail.com>
2018-12-12 09:13:36 +0800

    Finished result.display

--- bustw_cli/result.py
+++ bustw_cli/result.py
@@ -2,6 +2,7 @@ import readline
 
 from .utils.ask import ask
 from .utils.bustw import Bustw
+from .utils.less import print_less
 
 bustw = Bustw()
 
@@ -174,9 +175,72 @@ class Result:
             stop['stopStatus'] = time['stopStatus']
 
             real = self.__reals.get(stop['stopUID']) or []
-            stop['bus'] = real
+            stop['buses'] = real
 
-        print(info)
+    def display(self):
+        cities = self.__data['cities']
+        info = self.__info
+
+        result = ''
+        result += '\n'
+        result += '{0} {1}'.format(cities[info['city']]['name'], info['name'])
+        result += '\n'
+        result += "=" * 50
+        result += '\n'
+
+        for stop in info['stops']:
+            # 未發車
+            if stop['stopStatus'] == 1:
+                result += "\033[47m\033[30m[ 未發車 ]\033[0m "
+
+            # 交管不停
+            elif stop['stopStatus'] == 2:
+                result += "\033[43m\033[30m[交管不停]\033[0m "
+
+            # 末班駛離
+            elif stop['stopStatus'] == 3:
+                result += "\033[47m\033[30m[末班駛離]\033[0m "
+
+            # 今日不開
+            elif stop['stopStatus'] == 4:
+                result += "\033[47m\033[30m[今日不開]\033[0m "
+
+            # 有車
+            else:
+                # 有車且進站中
+                if len(stop['buses']) > 0 and 1 in list(map(lambda x: x['arriving'], stop['buses'])):
+                    result += "\033[41m\033[97m[ 進站中 ]\033[0m "
+
+                # 有車還沒進站
+                else:
+                    minute = stop['estimateTime'] // 60
+
+                    if minute > 2:
+                        result += '\033[44m\033[97m[ {0:>3} 分 ]\033[0m '.format(
+                            minute)
+                    else:
+                        result += '\033[45m\033[97m[ {0:>3} 分 ]\033[0m '.format(
+                            minute)
+
+            # 分析站名有多寬
+            length = 0
+            for char in stop['stopName']:
+                length += 2 if ord(char) > 127 else 1
+            # 顯示公車站名稱（最多 30 字）
+            result += stop['stopName'][:30] + ' ' * (30 - length)
+
+            # 如果有車的話顯示車號
+            if len(stop['buses']) > 0:
+                for bus in stop['buses']:
+                    # 有多台車就換行
+                    if stop['buses'].index(bus) != 0:
+                        result += " " * 41
+                    result += bus['busNumber']
+                    result += '\n'
+            else:
+                result += '\n'
+
+        print_less(result)
 
     def main(self):
         self.download_stops()
@@ -190,6 +254,7 @@ class Result:
             self.download_times()
 
             self.process()
+            self.display()
 
             self.__data['choice'] = self.__data['choice'][:2]
 
