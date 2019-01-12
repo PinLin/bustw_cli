a002aa6bdb5b5d642a333fe4fa938be79e044685
Pin Lin <moneycat711@gmail.com>
2019-01-12 17:44:57 +0800

    Rename variable name in bustw

--- bustw_cli/utils/bustw.py
+++ bustw_cli/utils/bustw.py
@@ -3,30 +3,30 @@ import requests
 
 class Bustw:
     def __init__(self):
-        self.__url = 'https://bus.ntut.com.tw/v1'
+        self.__base = 'https://bus.ntut.com.tw/v1'
 
-    def __fetch(self, url: str, **kwargs):
-        url = self.__url + url + '?ver=2'
+    def __get(self, resource: str, **kwargs):
+        url = self.__base + resource + '?ver=2'
         url = url.format(**kwargs)
 
         return requests.get(url).json()
 
     def get_city(self):
         """從伺服器取得城市資料"""
-        return self.__fetch('/city')
+        return self.__get('/city')
 
     def get_info(self, city: str, route: str=''):
         """從伺服器取得路線基本資料"""
-        return self.__fetch('/info/{city}/{route}', city=city, route=route)
+        return self.__get('/info/{city}/{route}', city=city, route=route)
 
     def get_stop(self, city: str, route: str):
         """從伺服器取得路線站牌資料"""
-        return self.__fetch('/stop/{city}/{route}', city=city, route=route)
+        return self.__get('/stop/{city}/{route}', city=city, route=route)
 
     def get_real(self, city: str, route: str):
         """從伺服器取得路線定位資料"""
-        return self.__fetch('/real/{city}/{route}', city=city, route=route)
+        return self.__get('/real/{city}/{route}', city=city, route=route)
 
     def get_time(self, city: str, route: str):
         """從伺服器取得路線時間資料"""
-        return self.__fetch('/time/{city}/{route}', city=city, route=route)
+        return self.__get('/time/{city}/{route}', city=city, route=route)
