6e2dc8d3d685ee47ed55634d55e860ea43569408
Pin Lin <moneycat711@gmail.com>
2019-01-13 18:24:27 +0800

    Go to MainView if file was downloaded

--- bustw_cli/views/init_view.py
+++ bustw_cli/views/init_view.py
@@ -1,5 +1,16 @@
 from .base_view import BaseView
 
+from ..utils.database import Database
+
+
 class InitView(BaseView):
+    """初始化"""
+
     def main(self):
-        return 'city'
+        with Database() as db:
+            # 判斷縣市及路線資料是否已下載
+            if len(db.select_city()):
+                return 'main'
+
+            else:
+                return 'city'
