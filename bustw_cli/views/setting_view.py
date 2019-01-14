1eacb210ce197ec263b0d957ba4628a52f23da60
Pin Lin <moneycat711@gmail.com>
2019-01-14 10:27:16 +0800

    Add setting for change cities we want

--- bustw_cli/views/setting_view.py
+++ bustw_cli/views/setting_view.py
@@ -1,6 +1,30 @@
 from .base_view import BaseView
 
+from PyInquirer import prompt
+
 
 class SettingView(BaseView):
     def main(self):
-        return 'main'
+        return self.select_function()
+
+    def select_function(self):
+        functions = {
+            '回到主畫面': 'main',
+            '修改要檢索的城市': 'city',
+        }
+
+        questions = [
+            {
+                'type': 'list',
+                'qmark': '🔧 ',
+                'name': 'choice',
+                'message': '請選擇要修改的設定\n',
+                'choices': functions.keys()
+            }
+        ]
+
+        print()
+        answer = prompt(questions)
+        print()
+
+        return functions[answer['choice']]
